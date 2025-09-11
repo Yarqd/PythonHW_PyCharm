da8a83493f786a9beff84abaf021625413fc7a43
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2025-09-11 19:00:03 +0300

    update program

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import concurrent
+
 spb_price = 20_000
 msc_price = 25_000
 ekb_price = 18_000
-# Набор правил форматирования называется pep8
+do_discount = True
+discount_value = 20  # В процентах
+
+dist = input("Введите направление (spb, msc, ekb): ")
+cnt = int(input("Введите количество людей: "))
+
+if dist == "spb":
+    dist_price = spb_price
+elif dist == "msc":
+    dist_price = msc_price
+elif dist == "ekb":
+    dist_price = ekb_price
+
+if not do_discount:
+    price = cnt * dist_price
+else:
+    price = cnt * dist_price * (100 - discount_value) // 100
+
+print("Итоговая цена" + str(price))
