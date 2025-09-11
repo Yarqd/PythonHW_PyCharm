b915b2c804f083335adfd93187acb76426c26f4e
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2025-09-11 19:11:48 +0300

    add kids

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import concurrent
-
 spb_price = 20_000
 msc_price = 25_000
 ekb_price = 18_000
@@ -7,7 +5,9 @@ do_discount = True
 discount_value = 20  # В процентах
 
 dist = input("Введите направление (spb, msc, ekb): ")
-cnt = int(input("Введите количество людей: "))
+adults_cnt = int(input("Введите количество взрослых: "))
+kids_cnt = int(input("Введите количество детей: "))
+cnt = 2 * adults_cnt + kids_cnt
 
 if dist == "spb":
     dist_price = spb_price
@@ -16,9 +16,8 @@ elif dist == "msc":
 elif dist == "ekb":
     dist_price = ekb_price
 
-if not do_discount:
-    price = cnt * dist_price
-else:
+price = cnt * dist_price // 2
+if do_discount:
     price = cnt * dist_price * (100 - discount_value) // 100
 
-print("Итоговая цена" + str(price))
+print("Итоговая цена " + str(price))
